2dc4d4ace83d3037c6a6d059964cde22e751ae2f
hicham-oualla <hich2022am.oualla@gmail.com>
2024-03-17 23:17:47 +0000

    calculate function

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
 let display = document.getElementById("display");
-let firstNumber = 0;
-function Display(num) {
-    firstNumber = num;
-    display.value += firstNumber;
-    
-}
-function clearDisplay(){
-    display.value = "  ";
+let firstNumber = "";
+let operator = "";
 
+function Display(num) {
+    firstNumber += num;
+    display.value += num;
 }
+
+function clearDisplay() {
+    display.value = "";
+    firstNumber = "";
+    operator = "";
+}
+
 function appendOperator(op) {
     display.value += op; 
-    step ++;
-    operation = op;
+    operator = op;
+}
+
+function appendDecimal() {
+    if (!firstNumber.includes(".")) {
+        firstNumber += ".";
+        display.value = firstNumber;
+    }
+}
+
+function calculate() {
+    if (operator === "" || firstNumber === "") {
+        return; 
+    }
+
+    let result;
+    const secondNumber = display.value.substring(display.value.indexOf(operator) + 1);
+    
+    switch (operator) {
+        case '+':
+            result = parseFloat(firstNumber) + parseFloat(secondNumber);
+            break;
+        case '-':
+            result = parseFloat(firstNumber) - parseFloat(secondNumber);
+            break;
+        case '*':
+            result = parseFloat(firstNumber) * parseFloat(secondNumber);
+            break;
+        case '/':
+            if (parseFloat(secondNumber) !== 0) {
+                result = parseFloat(firstNumber) / parseFloat(secondNumber);
+            } else {
+                result = "Error"; 
+            }
+            break;
+        default:
+            result = "Error";
+    }
+
+    display.value = result;
+    firstNumber = result.toString();
+    operator = "";
 }
